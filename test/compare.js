60ab89101b3d70f974ee64e82e7808cdf6d1de5e
Daniel Huigens <d.huigens@gmail.com>
2017-09-26 20:14:05 +0200

    Run every benchmark cycle in a fresh web worker

--- test/compare.js
+++ test/compare.js
@@ -31,6 +31,7 @@ var setupBenchmarks,
 parsers = [
 {
     name: 'Esprima',
+    src: new URL('../esprima.js', window.location).href,
     version: function () {
         return window.esprima.version;
     },
@@ -42,6 +43,7 @@ parsers = [
 {
     name: 'UglifyJS2',
     link: 'https://github.com/mishoo/UglifyJS2',
+    src: 'http://lisperator.net/s/js/uglifyjs/uglify.js',
     parse: function (code) {
         var syntax = window.UglifyJS.parse(code);
         return syntax.length;
@@ -50,6 +52,7 @@ parsers = [
 {
     name: 'Traceur',
     link: 'https://github.com/google/traceur-compiler',
+    src: 'http://google.github.io/traceur-compiler/bin/traceur.js',
     parse: function (code) {
         var file, pp, tree;
         file = new traceur.syntax.SourceFile('name', code);
@@ -61,6 +64,7 @@ parsers = [
 {
     name: 'Acorn',
     link: 'https://github.com/marijnh/acorn',
+    src: 'https://unpkg.com/acorn',
     version: function () {
         return window.acorn.version;
     },
@@ -72,8 +76,12 @@ parsers = [
 {
     name: 'Espree',
     link: 'https://github.com/eslint/espree',
+    src: 'http://eslint.org/js/app/eslint.js',
     version: function () {
         return window.espree.version;
+    },
+    setup: function () {
+        window.espree = require('espree');
     },
     parse: function (code) {
         var syntax = window.espree.parse(code, { range: true, loc: true });
@@ -83,6 +91,7 @@ parsers = [
 {
     name: 'Cherow',
     link: 'https://github.com/cherow/cherow',
+    src: 'https://unpkg.com/cherow',
     parse: function (code) {
         var syntax = window.cherow.parseScript(code, { ranges: true, locations: true });
         return syntax.body.length;
@@ -91,6 +100,7 @@ parsers = [
 {
     name: 'Shift',
     link: 'https://github.com/shapesecurity/shift-parser-js',
+    src: 'http://shift-ast.org/js/shift-parser.js',
     parse: function (code) {
         var syntax = window.parser.parseScriptWithLocation(code);
         return syntax.tree.statements.length;
@@ -99,6 +109,7 @@ parsers = [
 {
     name: 'Shift (no early errors)',
     link: 'https://github.com/shapesecurity/shift-parser-js',
+    src: 'http://shift-ast.org/js/shift-parser.js',
     parse: function (code) {
         var syntax = window.parser.parseScriptWithLocation(code, { earlyErrors: false });
         return syntax.tree.statements.length;
@@ -330,11 +341,36 @@ if (typeof window !== 'undefined') {
                 // Poor man's error reporter for Traceur.
                 console.reportError = console.error;
 
-                fn = function () {
-                    window.tree.push(pp.parse(source));
+                var worker;
+
+                fn = function (deferred) {
+                    worker = new Worker(URL.createObjectURL(new Blob([[
+                        'var window = self;',
+                        'importScripts("' + pp.src + '");',
+                        pp.setup ? '(' + pp.setup + ')();' : '',
+                        'addEventListener("message", function (event) {',
+                        '   var startTime = performance.now();',
+                        '   var result = (' + pp.parse + ')(event.data);',
+                        '   postMessage({',
+                        '       result: result,',
+                        '       elapsed: performance.now() - startTime,',
+                        '   });',
+                        '   close();',
+                        '});',
+                        'postMessage({message: "loaded"});'
+                    ].join('\n')], {type: 'text/javascript'})));
+                    worker.onmessage = function (event) {
+                        if (event.data.message === 'loaded') {
+                            worker.postMessage(source);
+                        } else {
+                            deferred.resolve();
+                            deferred.elapsed = event.data.elapsed / 1e3;
+                        }
+                    };
                 };
 
                 benchmark = new window.Benchmark(test, fn, {
+                    defer: true,
                     'onComplete': function () {
                         var str = '';
                         str += (1000 * this.stats.mean).toFixed(1) + ' \xb1';
@@ -346,13 +382,14 @@ if (typeof window !== 'undefined') {
                         }
                         totalTime[pp.name] += this.stats.mean;
                         setText(slug(pp.name) + '-total', (1000 * totalTime[pp.name]).toFixed(1) + ' ms');
+
+                        index += 1;
+                        window.setTimeout(run);
                     }
                 });
 
                 window.setTimeout(function () {
                     benchmark.run();
-                    index += 1;
-                    window.setTimeout(run, 211);
                 }, 211);
             }
 
