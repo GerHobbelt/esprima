17fa28db424cd949b168256a143dde55c5fc8f4e
Ger Hobbelt <ger@hobbelt.com>
2018-05-10 14:23:37 +0200

    bumped build revision, updated npm packages. All tests pass.

--- dist/esprima.js
+++ dist/esprima.js
@@ -203,7 +203,7 @@ exports.tokenize = tokenize;
 var syntax_1 = __webpack_require__(2);
 exports.Syntax = syntax_1.Syntax;
 // Sync with *.json manifests.
-exports.version = '4.0.1-21';
+exports.version = '4.0.1-22';
 
 
 /***/ }),
